7ed80a4f4bc5378f9e2680b6f05404f1aef31ba5
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-08-12 23:27:46 +0300

    Deleting functionality of Messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+import UpdateIcon from '@material-ui/icons/Update';
+
 import './Message.css';
+import { useParams } from 'react-router-dom';
+import db from '../firebase';
 
 function Message({ message, timestamp, user, id, userImage }) {
+  const { roomId } = useParams();
+  console.log(roomId);
+
+  //Deleting a message
+  const handleDeleteMessage = () => {
+    db.collection('room').doc(roomId).collection('messages').doc(id).delete();
+  };
+  //Upating a message
+  const handleUpdateMessage = () => {
+    console.log('updating');
+  };
+
   return (
     <div className="Message">
       <img src={userImage} alt={user} />
@@ -12,7 +29,17 @@ function Message({ message, timestamp, user, id, userImage }) {
             {new Date(timestamp?.toDate()).toUTCString()}
           </span>
         </h4>
-        <p>{message}</p>
+        <div className="Message-messageAndButtons">
+          <p>{message}</p>
+          <p>
+            <span>
+              <DeleteIcon onClick={handleDeleteMessage} />
+            </span>
+            <span>
+              <UpdateIcon onClick={handleUpdateMessage} />
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
