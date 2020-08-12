389b0e7963aca4b6b92bef9c0fddb04504315a65
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-08-13 00:59:08 +0300

    Updating  functionality of Messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,20 +5,47 @@ import UpdateIcon from '@material-ui/icons/Update';
 import './Message.css';
 import { useParams } from 'react-router-dom';
 import db from '../firebase';
+import { useState } from 'react';
+import firebase from 'firebase';
 
 function Message({ message, timestamp, user, id, userImage }) {
   const { roomId } = useParams();
-  console.log(roomId);
+  const [updateMessage, setUpdateMessage] = useState('');
+  const [toogleUpdate, setToogleUpdate] = useState(true);
 
   //Deleting a message
   const handleDeleteMessage = () => {
     db.collection('room').doc(roomId).collection('messages').doc(id).delete();
   };
-  //Upating a message
+  //
   const handleUpdateMessage = () => {
+    setToogleUpdate(false);
     console.log('updating');
+    db.collection('room')
+      .doc(roomId)
+      .collection('messages')
+      .doc(id)
+      .get()
+      .then((snap) => {
+        setUpdateMessage(snap.data().message);
+      });
   };
+  //Updating a message
+  const handleSubmitUpdateMessage = (e) => {
+    e.preventDefault();
 
+    db.collection('room')
+      .doc(roomId)
+      .collection('messages')
+      .doc(id)
+      .update({
+        message: updateMessage,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        setToogleUpdate(true);
+      });
+  };
   return (
     <div className="Message">
       <img src={userImage} alt={user} />
@@ -30,7 +57,21 @@ function Message({ message, timestamp, user, id, userImage }) {
           </span>
         </h4>
         <div className="Message-messageAndButtons">
-          <p>{message}</p>
+          {toogleUpdate ? (
+            <p>{message}</p>
+          ) : (
+            <form
+              onSubmit={handleSubmitUpdateMessage}
+              className="Message-updateMessage"
+            >
+              <input
+                type="text"
+                value={updateMessage}
+                onChange={(e) => setUpdateMessage(e.target.value)}
+              />
+            </form>
+          )}
+
           <p>
             <span>
               <DeleteIcon onClick={handleDeleteMessage} />
